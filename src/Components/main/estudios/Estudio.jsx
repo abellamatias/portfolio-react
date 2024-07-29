bbe255896d6cd5f935bb5a4b77d5069861bf9593
Matias Abella <matiasabella2000@gmail.com>
2024-07-29 15:36:45 -0300

    Listo con espaciado la sección Estudios

--- src/Components/main/estudios/Estudio.jsx
+++ src/Components/main/estudios/Estudio.jsx
@@ -2,11 +2,19 @@ import { Box, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Estudio =({title, content}) => {
+
+  let textoFormateado = content.split('\n').map((line, i) => (
+    <Text key={i}>
+        {line}
+        <br />
+    </Text>
+))
+
   return (
             
         <Box as='section' >
-            <Heading as='h4' fontSize='23px' m='0 0 16px 0'>{title} </Heading>
-            <Text m='0' fontSize='14'> {content} </Text>
+            <Heading as='h4' fontSize='23px' m='0 0 16px 0' fontWeight='500' fontFamily='rubik.general'>{title} </Heading>
+            <Text m='0' fontSize='14' lineHeight='1.15'> {textoFormateado} </Text>
         </Box>                    
     
   )
